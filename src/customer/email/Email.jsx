94b9b7cd728dff6829f6db913a144ce4ae83ef98
Khuong <khuong29102003@gmail.com>
2024-11-27 13:25:22 +0700

    change language

--- src/customer/email/Email.jsx
+++ src/customer/email/Email.jsx
@@ -20,22 +20,22 @@ const MailList = () => {
 
   return (
     <div className="w-full mt-[50px] bg-[#003580] text-white flex flex-col items-center gap-[20px] p-[50px]">
-      <h1 className="text-center text-[40px] font-medium">Save time, save money!</h1>
+      <h1 className="text-center text-[40px] font-medium">Tiết kiệm thời gian và tiền bạc!</h1>
 
       <span className="text-center text-[16px]">
-        Sign up and we'll send the best deals to you
+        Đăng ký và chúng tôi sẽ gửi cho bạn những ưu đãi tốt nhất
       </span>
       <form onSubmit={formik.handleSubmit}>
         <div className="max-w-[1100px] flex items-center h-[50px] system-font mb-2">
           <input className ="flex-1 h-full w-[400px] p-2 mr-2 rounded-md border-0 box-border text-black"
             type="text"
-            placeholder="Your Email"
+            placeholder="Email của bạn"
             name="email"
             onBlur={formik.handleBlur}
             onChange={formik.handleChange}
             value={formik.values.email}
           />
-          <button className="h-full w-auto bg-[#0071c2] text-white font-medium border-0 rounded-md cursor-pointer px-4 flex justify-center items-center">Subscribe</button>          
+          <button className="h-full w-auto bg-[#0071c2] text-white font-medium border-0 rounded-md cursor-pointer px-4 flex justify-center items-center">Đặt ngay</button>          
         </div>
         {formik.touched.email && formik.errors.email && (
           <div className="text-red-500 text-[18px] mt-0 font-medium">{formik.errors.email}</div>
--- src/customer/footer/Footer.jsx
+++ src/customer/footer/Footer.jsx
@@ -4,46 +4,46 @@ const Footer = () => {
       <div className="w-full max-w-[1100px]">
         <div className="w-full flex justify-between mb-[50px] flex-wrap">
           <ul className="list-none p-0">
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Countries</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Regions</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Cities</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Districts</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Airports</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Hotels</li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Các quốc gia</li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Khu vực</li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Thành phố</li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Quận/Huyện</li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Sân bay</li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Khách sạn</li>
           </ul>
           <ul className="list-none p-0">
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Homes </li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Apartments </li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Resorts </li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Villas</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Hostels</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Guest houses</li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Nhà riêng </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Căn hộ </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Khu nghỉ dưỡng </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Biệt thự</li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Nhà trọ</li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Nhà khách</li>
           </ul>
           <ul className="list-none p-0">
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Unique places to stay </li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Reviews</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Unpacked: Travel articles </li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Travel communities </li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Seasonal and holiday deals </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Những nơi lưu trú độc đáo </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Đánh giá </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Unpacked: Bài viết du lịch </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Cộng đồng du lịch </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Ưu đãi theo mùa và ngày lễ </li>
           </ul>
           <ul className="list-none p-0">
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Car rental </li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Flight Finder</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Restaurant reservations </li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Travel Agents </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Thuê xe </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Tìm chuyến bay </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Đặt chỗ nhà hàng </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Đại lý du lịch </li>
           </ul>
           <ul className="list-none p-0">
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Curtomer Service</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Partner Help</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Careers</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Sustainability</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Press center</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Safety Resource Center</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Investor relations</li>
-            <li className="mb-[10px] text-[#003580] cursor-pointer">Terms & conditions</li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Dịch vụ khách hàng </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Hỗ trợ đối tác </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Tuyển dụng </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Phát triển bền vững </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Trung tâm báo chí </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Trung tâm tài nguyên an toàn </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Quan hệ nhà đầu tư </li>
+            <li className="mb-[10px] text-[#003580] cursor-pointer">Điều khoản và điều kiện </li>
           </ul>
         </div>
-        <div className="fText">Copyright © 2024 TravelBK.</div>
+        <div className="fText">Bản quyền © 2024 TravelBK.</div>
       </div>
     </div>
   );
